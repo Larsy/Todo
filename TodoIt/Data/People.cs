211fbb3c16267d153f32958c8e37e0ebcaaa31f9
Larsy <lars@larsy.net>
2021-07-07 23:54:30 +0200

    Ny funktion för att Ta bort ett Todo med visst todoID, fixad id-bugg och bättre clear-metod

--- TodoIt/Data/People.cs
+++ TodoIt/Data/People.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using TodoIt.Model;
 
 
@@ -12,24 +13,37 @@ namespace TodoIt.Data
         {
             return Person.Length;
         }
+
         public static Person[] FindAll()
         {
             return Person;
         }
+
         public static Person FindById(int personId)
         {
-            return Person[personId];
+            int indexToFind;
+            indexToFind = Array.FindIndex(Person, x => x.PersonId == personId);
+            return Person[indexToFind];
         }
-        public static Person AddPerson(string firstName, string middleName, string lastName, int age, int gender, bool vaccinated)
+
+        public static Person AddPerson(string firstName, string middleName, string lastName, int age, object gender, bool vaccinated)
         {
             Person personToBeAdded = new Person(firstName, middleName, lastName, age, gender, vaccinated);
             Array.Resize(ref Person, Size() + 1);
             Person[^1] = personToBeAdded;
             return Person[^1];
         }
+
+        public static void RemovePersonById(int personId)
+        {
+            int indexToRemove;
+            indexToRemove = Array.FindIndex(Person, x => x.PersonId == personId);
+            Person = Person.Where((source, index) => index != indexToRemove).ToArray();
+        }
+
         public static void Clear()
         {
-            Array.Empty<Person>();
+            Person = Array.Empty<Person>();
         }
     }
 }
