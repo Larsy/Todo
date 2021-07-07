4a04539c90973f4950a7018a7c74820c7a8efc17
Larsy <lars@larsy.net>
2021-07-07 23:50:59 +0200

    Uppdaterad för att matcha Propertyförbättring

--- TodoIt/Program.cs
+++ TodoIt/Program.cs
@@ -14,21 +14,21 @@ namespace TodoIt
             Console.WriteLine("Efternamn:".PadRight(15) + person1.LastName);
             Console.WriteLine();
  
-            Person person2 = new Person("Tessan", "Testy", "Testsson", 51, (int)Genders.Female, true);
+            Person person2 = new Person("Tessan", "Tessy", "Testorsson", 51, Genders.Female, true);
             
             Console.WriteLine("Id".PadRight(15) + person2.PersonId);
             Console.WriteLine("Namn:".PadRight(15) + person2.FullName);
             Console.WriteLine("Ålder:".PadRight(15) + person2.Age);
-            Console.WriteLine("Kön:".PadRight(15) + Enum.GetName(typeof(Genders), person2.Gender));
+            Console.WriteLine("Kön:".PadRight(15) + person2.Gender);
             Console.WriteLine("Vaccinerad:".PadRight(15) + person2.Vaccinated);
             Console.WriteLine();
 
-            Person person3 = new Person("Testor", "Testy", "Testorsson", 54, (int)Genders.Male, true);
+            Person person3 = new Person("Testor", "Testy", "Testorsson", 54, Genders.Male, true);
 
             Console.WriteLine("Id".PadRight(15) + person3.PersonId);
             Console.WriteLine("Namn:".PadRight(15) + person3.FullName);
             Console.WriteLine("Ålder:".PadRight(15) + person3.Age);
-            Console.WriteLine("Kön:".PadRight(15) + Enum.GetName(typeof(Genders), person3.Gender));
+            Console.WriteLine("Kön:".PadRight(15) + person3.Gender);
             Console.WriteLine("Vaccinerad:".PadRight(15) + person3.Vaccinated);
         }
     }
