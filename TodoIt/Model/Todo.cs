a7cf8b24209d639547baa90e778351f187cce4b0
Larsy <lars@larsy.net>
2021-07-08 08:21:18 +0200

    Avstånd mellan metoder

--- TodoIt/Model/Todo.cs
+++ TodoIt/Model/Todo.cs
@@ -15,32 +15,38 @@ namespace TodoIt.Model
         {
             get { return todoId; }
         }
+
         public string Description
         {
             get { return description; }
             private set { description = value; }
         }
+
         public bool Done
         {
             get { return done; }
             private set { done = value; }
         }
+
         public Person Assignee
         {
             get { return assignee; }
             private set { assignee = value; }
         }
+
         public Todo (string description)
         {
             todoId = TodoSequencer.nextTodoId();
             Description = description;
         }
+
         public Todo (string description, bool done)
         {
             todoId = TodoSequencer.nextTodoId();
             Description = description;
             Done = done;
         }
+
         public Todo (Person assignee, string description, bool done)
         {
             todoId = TodoSequencer.nextTodoId();
